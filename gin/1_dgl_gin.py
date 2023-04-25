9ebf731bc6b9d09a66d16c54ea22fcfe8c8693a4
yuke_wang@ucsb.edu <yuke_wang@ucsb.edu>
2023-04-25 21:26:34 +0000

    update with benchscripts

--- gin/1_dgl_gin.py
+++ gin/1_dgl_gin.py
@@ -19,7 +19,7 @@ def main():
             ( 'Reddit'                      , 602      	, 41),
             ( 'enwiki-2013'	                , 300	    , 12),   
             ( 'it-2004'                     , 256       , 64),
-            # ( 'paper100M'                   , 96       , 64),
+            # ( 'paper100M'                   , 128       , 64),
             ( 'ogbn-products'	            , 100	    , 47),   
             ( 'ogbn-proteins'	            , 8		    , 112),
             ( 'com-Orkut'		            , 128		, 32),
